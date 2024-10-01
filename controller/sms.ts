90edd4cc6ecea46bc569999616292901c7027a66
kavehjamshidi <kaveh.jamshidi98@gmail.com>
2024-10-01 12:35:33 +0330

    update files

--- controller/sms.ts
+++ controller/sms.ts
@@ -10,7 +10,7 @@ export default function (fastify: any, opts: any, done: any) {
   cron.schedule(cronSchedule, async () => {
     // const {changeCronSchedule} = await services.smsSender(fastify.req, fastify.reply)
 
-    console.log(111111111111)
+    console.log(111111111111, cronSchedule)
     const changeCronSchedule = true
     changeCronSchedule ? cronSchedule = `*/10 * * * * *` : null
   });
